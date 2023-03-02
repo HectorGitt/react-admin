3dc312ee0214f6f9d30d5440c7744c238bbad46f
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2023-03-02 12:32:44 +0100

    removed unused vars

--- src/scenes/faq/index.js
+++ src/scenes/faq/index.js
@@ -1,14 +1,11 @@
-import {Box, useTheme, Typography} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import Header from '../../components/Header';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { tokens } from '../../theme';
 
 const Faq = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
 
     return (
         <Box m="20px">
